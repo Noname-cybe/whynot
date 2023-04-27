f9120bead206d6d6181aea17e26c30d7337292ae
Noname-cybe <yucel.fabiano@gmail.com>
2023-04-27 20:15:44 +0700

    first commit

--- NoName.py
+++ NoName.py
@@ -12,12 +12,12 @@ main()
 text = "Selamat , datang di NoName !! -->"
 for char in text:
     print(char, end='', flush=True)
-    time.sleep(0,1)
+    time.sleep(0.1)
 print()
 
-print("-->Hacking is caring")
+print("-->Hacking is caring<--")
 text = [
-    "--> Kami berjalan bersama rakyat <--",
+    "--> Kami berjalan bersama rakyat <--"
     "Peraturan seharusnya memberikan solusi , bukan merugikan rakyat kecil",
     "Jadi jangan seenaknya !!!"
 ]
@@ -25,21 +25,22 @@ text = [
 for line in text:
     for char in line:
         print(char, end='', flush=True)
-        time.sleep(0,1)
-
+        time.sleep(0.1)
+    print()
+    
 def random_name_generator():
-    first_name =["apakah nama kamu haryanto", "apakah nama kamu mansur"]
-    last_name =["jasoon ..?" , "sandy..?" , "jones..?" , "fredy ..?"]
+    first_name =["apakah nama kamu haryanto", "apakah nama kamu mansur", "apakah nama janet"]
+    last_name =["jason ..?" , "sandy..?" , "jones..?" , "fredy ..?"]
     full_name = random.choice(first_name) +" "+ random.choice(last_name)
     return full_name
 
 def main():
     while True:
-        print("gas ajad dulu curhat belkangan...")
+        print("gas ajad dulu curhat belakangan...")
         time.sleep(2)
-        print("Bersiaplah untuk keadilan yg sebenarnya"), random_name_generator()
+        print("Bersiaplah untuk keadilan yg sebenarnya !!"), random_name_generator()
 
-        again = input("mau ganti nama g bree (ya/no)?")
+        again = input("mau ganti nama ga bree (ya/no)?")
         if again.lower() != "ya":
             break
 
